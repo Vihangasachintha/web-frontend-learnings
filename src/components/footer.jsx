35de10482f8bebe6d4d2b4ca39e230d04832068c
Vihanga Sachintha <vihanga1sachintha@gmail.com>
2026-02-22 13:31:53 +0530

    fix: refactor Footer component layout for improved structure and styling

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -12,22 +12,48 @@ export default function Footer() {
 
   return (
     <footer className="w-full mt-8">
-      <div className=" w-full h-[200px] bg-pink-600 pt-15">
-        <div>
-          <div></div>
-          <div
-            className="w-[30%] pl-15 text-white"
-            style={{ display: "flex", gap: "20px", fontSize: "24px" }}
-          >
-            <FaFacebookF className={iconStyle} />
-            <FaPinterestP className={iconStyle} />
-            <FaInstagram className={iconStyle} />
-            <SiTiktok className={iconStyle} />
-            <FaYoutube className={iconStyle} />
-            <SiThreads className={iconStyle} />
-          </div>
+      <div className=" w-full h-[200px] bg-pink-600 flex">
+        <div className="w-[30%] flex justify-center items-center">
+          <img
+            src="/logo.png"
+            alt="Logo"
+            className="w-[100px] h-[100px] object-contain hover:scale-125 transition-transform duration-300"
+          />
+        </div>
+        <div
+          className="w-[30%] pl-15 text-white pt-22"
+          style={{ display: "flex", gap: "20px", fontSize: "24px" }}
+        >
+          <FaFacebookF className={iconStyle} />
+          <FaPinterestP className={iconStyle} />
+          <FaInstagram className={iconStyle} />
+          <SiTiktok className={iconStyle} />
+          <FaYoutube className={iconStyle} />
+          <SiThreads className={iconStyle} />
+        </div>
+        <div className="w-[30%] pt-10 text-white pl-35">
+          <h2 className="mb-2">CONTACT US</h2>
+          <address className="not-italic space-y-1 text-sm">
+            <p>Negombo, Sri Lanka</p>
+            <p>
+              Email:
+              <a
+                href="mailto:support@velora.com"
+                className="hover:underline"
+              >
+                support@velora.com
+              </a>
+            </p>
+            <p>
+              Phone:
+              <a href="tel:+94771234567" className="hover:underline">
+                +94 77 1234567
+              </a>
+            </p>
+          </address>
         </div>
       </div>
+
       <div className="w-full h-[50px] bg-pink-600 border-t-1 border-gray-400 flex items-center justify-center">
         <p className="text-white">
           Copyright © 2026 Velora.lk | All rights reserved. | Design By
