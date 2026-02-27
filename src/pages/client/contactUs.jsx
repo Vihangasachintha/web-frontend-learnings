17d81028408a281eb6afc281b1b04a898aeee632
Vihanga Sachintha <vihanga1sachintha@gmail.com>
2026-02-27 11:48:37 +0530

    feat: add ContactUsPage component with contact information and support details

---
+++ src/pages/client/contactUs.jsx
@@ -0,0 +1,58 @@
+export default function ContactUsPage() {
+  return (
+    <div className="w-full h-full flex flex-col items-center justify-center">
+      <div className="md:w-[75%] sm:w-full w-[90%] lg:w-[60%] mt-8">
+        <h1 className="text-3xl text-secondary font-bold mb-4 text-center">
+        Contact Us
+      </h1>
+      <h2 className="text-xl mb-2">Get in Touch with Velora</h2>
+      <p>
+        We’re here to help you with anything you need — from product inquiries
+        to order support. Our team is always ready to assist you and make your
+        shopping experience smooth and enjoyable.{" "}
+      </p>
+      <h2 className="text-lg mt-3">Our Office</h2>
+      <p>
+        Velora (Pvt) Ltd <br />
+        No. 25, Flower Road, <br />
+        Colombo 07, <br />
+        Sri Lanka{" "}
+      </p>
+
+      <h2 className="text-lg mt-3">Phone</h2>
+      <p>
+        +94 77 123 4567 <br />
+        +94 11 234 5678 <br />
+        <br />
+      </p>
+      <h2 className="mb-2 text-lg">Customer Support Hours:</h2>
+      <p>
+        Monday – Friday: 9.00 AM – 6.00 PM <br />
+        Saturday: 9.00 AM – 2.00 PM{" "}
+      </p>
+
+      <h2 className="mt-3 text-lg mb-2">Email</h2>
+      <p>
+        General Inquiries: support@velora.lk <br />
+        Sales: sales@velora.lk <br />
+        Returns & Complaints: returns@velora.lk
+      </p>
+
+      <h2 className="mb-2 text-lg mt-3">Follow Us</h2>
+      <p>
+        Facebook: facebook.com/velora <br />
+        Instagram: @velora.lk <br />
+        TikTok: @velorabeauty
+      </p>
+
+      <h2 className="mt-3 text-lg mb-2">Customer Support</h2>
+      <p>
+        Need help choosing the right product? <br /> Our beauty advisors are
+        happy to guide you to find the perfect match for your skin and
+        lifestyle. <br />
+        <br /> We aim to respond to all inquiries within 24 hours.
+      </p>
+      </div>
+    </div>
+  );
+}
